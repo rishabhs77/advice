2543bc74f95e7c068cd0c34a96bd78e6ddbb1fce
Rishabh Sareen <rishabh.sareen@dbtechhackathon.com>
2021-04-24 21:28:36 +0530

    calling advice api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(()=>{
+    fetchMyAdvice();
+  },[]);
+
+  const [advice,setMyadvice]=useState("");
+  const [author,setMyauthor]=useState("");
+
+
+  const fetchMyAdvice = async ()=>{
+    try {
+      const response=await axios.get("https://goquotes-api.herokuapp.com/api/v1/random?count=1");
+      const {text,author}=response.data.quotes[0];
+      setMyadvice(text);
+      setMyauthor(author)
+      
+    } catch (error) {
+      console.log(error);
+
+    }
+    
+  };
+
+  /*const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      const {advice}=data.slip;
+      console.log(advice)
+
+    } catch (error) {
+      console.log(error); 
+
+    }
+  };*/
+
   return (
     <div className="App">
-      <h1>HI New Project</h1>
+      <h1>{advice}</h1>
+      <h2>{`by ${author}`}</h2>
+      <button onClick={fetchMyAdvice}>Get Advice</button>
     </div>
   );
 }
